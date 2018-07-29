b5991b1948ec327fc7f88b944ce812c733977ba3
JiangLirui <535904200@qq.com>
2018-07-29 13:49:10 +0800

    modify the css and add hot on APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './component/search_bar';
+// 引入HMR
+import { hot } from 'react-hot-loader'
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        122<SearchBar />
       </div>
     );
   }
 }
+
+export default hot(module)(App)
