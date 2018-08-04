850fccd812969ea9cb2d047341aeb5588a432082
JiangLirui <535904200@qq.com>
2018-08-04 16:27:12 +0800

    add song_info component

--- src/component/song_info/index.tsx
+++ src/component/song_info/index.tsx
@@ -1,13 +1,40 @@
-import { Component } from "react";
+import React from "react";
+import { connect } from 'react-redux';
 
-interface songInfoProps {
-
+interface songAlbum {
+  id:number;
+  name:string;
+  picUrl:string;
+  pic:number;
+}
+interface songArtist {
+  id:number;
+  name:string;
+}
+export interface songInfoProps {
+  name:string;
+  id:number;
+  ar:songArtist;
+  al:songAlbum;
 }
 
 interface songInfoState {
 
 }
 
-class songInfo extends Component {
-  
+class songInfo extends React.Component<songInfoProps, songInfoState> {
+  render() {
+    return (
+      <div>
+        <span>{this.props.name}</span>
+        <span>{this.props.ar.name}</span>
+        <span>选择</span>
+        <span>收藏</span>
+        <span>添加到列表</span>
+        <span>查看专辑</span>
+      </div>
+    )
+  }
 }
+
+export default connect((state) => ({}),{})(songInfo)
