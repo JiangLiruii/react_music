e829a071276f4aa46b72417648ba518c57aa1057
JiangLirui <535904200@qq.com>
2018-08-06 18:35:41 +0800

    update the port params in node env

--- public/server.js
+++ public/server.js
@@ -1,14 +1,15 @@
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
 var config = require('../webpack.config')
+var port = process.env.PORT || 3001
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   noInfo: false,
   historyApiFallback: true
-}).listen(3000, '127.0.0.1', function (err, result) {
+}).listen(port, '127.0.0.1', function (err, result) {
   if (err) {
     console.log(err)
   }
-  console.log('Listening at localhost:3000')
+  console.log(`Listening at localhost:${port}`)
 })
