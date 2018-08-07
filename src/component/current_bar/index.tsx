521ef1ca2f6f37a95c30fee9edd6bfd3a5b37812
JiangLirui <535904200@qq.com>
2018-08-07 14:31:57 +0800

    make play button to be responsive

--- src/component/current_bar/index.tsx
+++ src/component/current_bar/index.tsx
@@ -34,6 +34,15 @@ class CurrentBar extends React.Component<CurrentBarProps, CurrentBarState> {
   private _onVolumeChange() {
 
   }
+  public componentDidMount() {
+    const audio = this.playAudio.current;
+    audio.oncanplay = () => {
+      audio.play();
+      this.setState({
+        playing:true,
+      });
+    };
+  }
   private _onPlayClick() {
     this.state.playing ? this.playAudio.current.pause() : this.playAudio.current.play();
     this.setState({
@@ -41,11 +50,13 @@ class CurrentBar extends React.Component<CurrentBarProps, CurrentBarState> {
     });
   }
   public render() {
+    console.log(this.playAudio.current && this.playAudio.current.paused);
     return (
       <div className="current_song">
-        <audio src={this.props.play_url} autoPlay={true} ref={this.playAudio}></audio>
+        <audio src={this.props.play_url} autoPlay={false} ref={this.playAudio}></audio>
         <span className="before"></span>
-        <span className={this.state.playing ? 'pause' : 'play'} onClick={this._onPlayClick}></span>
+        <span className={this.playAudio.current && (this.state.playing ? 'pause' : 'play')}
+          onClick={this._onPlayClick}></span>
         <span className="next"></span>
         <input type="range" name="play_range" min="0" max={this.props.timelength} step="1" value={this.state.currentTime} onChange={this._onPlayChange} />
         <input type="range" name="volume" min="0" max="100" step="1" value={this.state.currentVolume} onChange={this._onVolumeChange} />
