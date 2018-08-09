a033c945f5ce9b25b8ec7d2ff7f3523ca96e3436
JiangLirui <535904200@qq.com>
2018-08-09 18:11:36 +0800

    make request to be configed

--- router/search.js
+++ router/search.js
@@ -1,9 +1,12 @@
 const request = require('superagent');
 
 module.exports = (req, res) => {
-  const keyword = req.query.keyword;
-  console.log(keyword);
-  request('http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword='+ encodeURIComponent(keyword) + '&page=1&pagesize=90&showtype=2').then(
+  const {
+    name,
+    page,
+    pagesize
+  } = req.query;
+  request(`http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword=${encodeURIComponent(name)}&page=${page}&pagesize=${pagesize}&showtype=2`).then(
     result => {
       const info = JSON.parse(result.text).data.info;
       res.send(info);
