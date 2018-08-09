0db9fc0c36f4e730931affcb569a54e2ed68e2de
JiangLirui <535904200@qq.com>
2018-08-09 18:14:13 +0800

    redispatch when song search

--- src/reducer/song_list.ts
+++ src/reducer/song_list.ts
@@ -1,17 +1,26 @@
 import { handleActions } from 'redux-actions';
 import { SongInfo } from './song_single';
+import request from 'superagent';
 
 export interface SongsState {
   songs_list:SongInfo[];
+  currentMusicState:Query;
 }
 
 const FETCH_MUSICS = 'music/FETCH_MUSICS';
-
+const CHANGE_STATE = 'music/CHANGE_STATE';
 const initalState:SongsState = {
   songs_list: [],
+  currentMusicState:{name:'', page:1, pagesize:20},
 };
 
-export function fetchMusicsActionCreator(songs_list:string[]) {
+interface Query {
+  name:string;
+  page?:number;
+  pagesize?:number;
+}
+
+export function fetchMusicsAsyncActionCreator(query:Query) {
   /**
    * 接口说明
    * type:
@@ -31,6 +40,22 @@ export function fetchMusicsActionCreator(songs_list:string[]) {
    *    1009 主播电台
    *    后跟s=xxx表示关键字, offset第几页 limit一页返回多少首歌曲 type=search&s=helloworld&offset=2&limit=20
    */
+  const {name, page, pagesize} = query;
+  return (dispatch) => {
+    dispatch(changeCurrentMusicState(query));
+    request.get(`http://localhost:3003/search?name=${name}&page=${page}&pagesize=${pagesize}`)
+  .then((res) => {
+    dispatch(fetchMusicSyncActionCreator(res.body));
+  });
+  };
+}
+function changeCurrentMusicState(currentMusicState:Query) {
+  return {
+    type: CHANGE_STATE,
+    payload: {currentMusicState},
+  };
+}
+export function fetchMusicSyncActionCreator(songs_list) {
   return {
     type: FETCH_MUSICS,
     payload: {songs_list},
@@ -39,10 +64,13 @@ export function fetchMusicsActionCreator(songs_list:string[]) {
 
 export default handleActions({
   [FETCH_MUSICS]: (state, action) => {
-    console.log(action);
     return {
       ...state,
       songs_list: action.payload.songs_list,
     };
   },
+  [CHANGE_STATE]: (state, action) => ({
+    ...state,
+    currentMusicState: action.payload.currentMusicState,
+  }),
 }, initalState);
