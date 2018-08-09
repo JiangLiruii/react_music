26c3fe723ce202b1ee494d5009c4c51da17e111e
JiangLirui <535904200@qq.com>
2018-08-09 18:12:05 +0800

    add middleware in store to realize async dispatch

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Reducer from './reducer';
-
-const store = createStore(Reducer);
+const loggerMiddleware = createLogger();
+const store = createStore(Reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 ReactDOM.render(
 <Provider store={store}>
