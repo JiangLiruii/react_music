21889e683c5d82287a96251ca448cef604aa99e6
JiangLirui <535904200@qq.com>
2018-08-11 23:28:01 +0800

    add app state for different state to page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,24 @@ import CurrentBar from './component/current_bar';
 import Nav from './component/navigator';
 import CSSModule from 'react-css-modules';
 
+/**
+ * current_show:
+ *  1 play_list
+ *  2 search_list
+ *  3 lyrics and song img
+ */
+interface AppStates {
+  current_show:number;
+}
+
 @CSSModule(require('./App.css'))
-export default class App extends React.Component {
+export default class App extends React.Component<{},AppStates> {
+  public constructor(props:any) {
+    super(props);
+    this.state = {
+      current_show: 1,
+    };
+  }
   public render() {
     return (
       <div styleName="App">
