fd465d20a0db463251d692a72cd1d8da1fd947fa
JiangLirui <535904200@qq.com>
2018-08-12 21:14:31 +0800

    add playlist and songlyrics components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SearchBar from './component/search_bar';
 import SongList from './component/songs_list';
+import PlayList from './component/play_list';
+import SongLyrics from './component/lyric';
 import CurrentBar from './component/current_bar';
 import Nav from './component/navigator';
 import CSSModule from 'react-css-modules';
@@ -16,19 +18,28 @@ interface AppStates {
 }
 
 @CSSModule(require('./App.css'))
-export default class App extends React.Component<{},AppStates> {
+export default class App extends React.Component<{}, AppStates> {
   public constructor(props:any) {
     super(props);
     this.state = {
       current_show: 1,
     };
+    this._onNavClick = this._onNavClick.bind(this);
+  }
+  private _onNavClick(index:number) {
+    this.setState({
+      current_show: index,
+    });
   }
   public render() {
+    const current_show = this.state.current_show;
     return (
       <div styleName="App">
         <SearchBar />
-        <Nav />
-        <SongList />
+        <Nav clickFunc={this._onNavClick}/>
+        {current_show === 1 && <PlayList />}
+        {current_show === 2 && <SongList />}
+        {current_show === 3 && <SongLyrics />}
         <CurrentBar />
       </div>
     );
