a34631551bfdb5dbf532ed2f84bb9a7d848d84a5
JiangLirui <535904200@qq.com>
2018-08-12 21:15:07 +0800

    update navigator

--- src/component/navigator/index.tsx
+++ src/component/navigator/index.tsx
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import CSSModule from 'react-css-modules';
 
+interface NavigatorProps {
+  clickFunc:Function;
+}
 interface NavigatorStates {
   choose_index:number;
 }
 @CSSModule(require('./index.scss'))
-export default class Navigator extends Component<{}, NavigatorStates> {
+export default class Navigator extends Component<NavigatorProps, NavigatorStates> {
   constructor(props) {
     super(props);
     this.state = {
       choose_index: 1,
     };
+    this._onNavClick = this._onNavClick.bind(this);
+  }
+  private _onNavClick (index:number) {
+    this.props.clickFunc(index);
+    this.setState({
+      choose_index: index,
+    });
   }
   public render() {
     return (
       <div styleName="navigator">
-        <span styleName={this.state.choose_index === 1 ? 'active' : 'normal'} onClick={() => this.setState({choose_index: 1})}>播放列表</span>
-        <span styleName={this.state.choose_index === 2 ? 'active' : 'normal'} onClick={() => this.setState({choose_index: 2})}>搜索结果</span>
-        <span styleName={this.state.choose_index === 3 ? 'active' : 'normal'} onClick={() => this.setState({choose_index: 3})}>当前歌曲</span>
+        <span styleName={this.state.choose_index === 1 ? 'active' : 'normal'} onClick={() => this._onNavClick(1)}>播放列表</span>
+        <span styleName={this.state.choose_index === 2 ? 'active' : 'normal'} onClick={() => this._onNavClick(2)}>搜索结果</span>
+        <span styleName={this.state.choose_index === 3 ? 'active' : 'normal'} onClick={() => this._onNavClick(3)}>当前歌曲</span>
       </div>
     );
   }
