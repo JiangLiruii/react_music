543b1f19abb47cabd83f80e5e29984caebdad774
JiangLirui <535904200@qq.com>
2018-08-22 18:32:50 +0800

    fix looooooong song name display bug

--- src/component/current_bar/index.tsx
+++ src/component/current_bar/index.tsx
@@ -160,7 +160,8 @@ class CurrentBar extends React.Component<CurrentBarProps, CurrentBarState> {
         <span styleName="next" onClick={this._onNextClick}></span>
         <div styleName="audio_bar">
           <input type="range" name="play_range" min="0" max={this.props.timelength / 1000} step="1" value={this.state.currentTime} onChange={this._onPlayChange} />
-          <span styleName="song_name">{(this.props.song_name || '暂无歌曲') + ' 比特率: ' + this.props.bitrate}</span>
+          <span styleName="song_name">{this.props.song_name || '暂无歌曲'}</span>
+          <span styleName="bitrate">{'比特率: ' + this.props.bitrate}</span>
         </div>
         <div styleName="mode">
           <div styleName={this.state.mode} onClick={this._onModeClick}></div>
