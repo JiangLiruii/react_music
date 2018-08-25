f055fbb829357e2a6c154a874bf0ca6771c1441a
JiangLirui <535904200@qq.com>
2018-08-25 18:26:41 +0800

    differ from dev or prod in main entry

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,19 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Reducer from './reducer';
+import registerSw from './registerServiceWorker';
 let loggerMiddleware;
 let store;
 if (process.env.NODE_ENV === 'development') {
   loggerMiddleware = createLogger();
+  store = createStore(Reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+} else {
   store = createStore(Reducer, applyMiddleware(thunkMiddleware));
 }
-store = createStore(Reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 ReactDOM.render(
 <Provider store={store}>
   <App />
 </Provider>, document.getElementById('root'));
-registerServiceWorker();
+registerSw();
