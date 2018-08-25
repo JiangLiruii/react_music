251450045649cf52cbbe53f61abfb7893a9cd821
JiangLirui <535904200@qq.com>
2018-08-25 14:53:34 +0800

    minify webpack config and add code split, updateoptimizecssassetsplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const devMode = process.env.NODE_ENV !== 'production';
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-// 分离css
+// // 分离css
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const cssnano = require('cssnano');
 // 引入favicon
 const FaviconWebpackPlugin = require('favicons-webpack-plugin');
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: [
-    // 开启react代码的模块热替换（HMR）, 必不可少
-    'react-hot-loader/patch',
+    // // 开启react代码的模块热替换（HMR）, 必不可少
+    // 'react-hot-loader/patch',
     // 指定域名与端口
     `webpack-dev-server/client?http://0.0.0.0:${process.env.PORT || 3001}`,
     // 为HMR打包好运行代码, only表示只在更新成功后进行HMR
-    'webpack/hot/only-dev-server',
+    // 'webpack/hot/only-dev-server',
     // app入口文件
     './src/index'
 
@@ -49,7 +49,8 @@ module.exports = {
       test: /\.scss$/,
       // css-loader会遍历css文件，找到所有的url(...)并且处理。style-loader会把所有的样式插入到你页面的一个style tag中。
       use: [
-        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        // devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'style-loader',
         {
           loader: 'css-loader',
           options: {
@@ -79,28 +80,59 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + '/index.html'
     }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    // 开启全局HMR
-    new webpack.HotModuleReplacementPlugin(),
-    // console输出更友好的模块名称,可以知道热重载时是哪个模块作出了变动
-    new webpack.NamedModulesPlugin(),
-    new FaviconWebpackPlugin('./favicon-16x16.png'),
+    // // 开启全局HMR
+    // devMode && (new webpack.HotModuleReplacementPlugin()),
+    // // console输出更友好的模块名称,可以知道热重载时是哪个模块作出了变动
+    // devMode && (new webpack.NamedModulesPlugin()),
+    new FaviconWebpackPlugin({logo: './favicon-16x16.png',background: '#fff', theme_color: '#fff'}),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: cssnano,
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true,
+        },
+        safe: false,
+      },
+      canPrint: false,
+    })
   ],
   node: {
     fs: "empty",
     tls:"empty"
  },
   optimization: {
+    splitChunks: {
+      // 对异步块进行优化
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        venders: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        }
+      }
+    },
     minimizer: [
       new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true // set to true if you want JS source maps
-      }),
-      new OptimizeCSSAssetsPlugin({})
+        sourceMap: false,
+        uglifyOptions: {
+          compress: {
+            // 去掉所有的console
+            drop_console: true,
+          }
+        }
+      })
     ]
   },
 }
