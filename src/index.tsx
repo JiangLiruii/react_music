d1e8976fe5aabe32ba879e183a5f12f16d1a77a8
JiangLirui <535904200@qq.com>
2018-08-30 19:27:53 +0800

    for chrome redux plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './App';
 import Reducer from './reducer';
 import registerSw from './registerServiceWorker';
-let loggerMiddleware;
 let store;
+
 if (process.env.NODE_ENV === 'development') {
-  loggerMiddleware = createLogger();
-  store = createStore(Reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
+  const composeEnhancers =
+  typeof window === 'object' &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+  const enhancer = composeEnhancers(
+    applyMiddleware(thunkMiddleware),
+  );
+  store = createStore(Reducer, enhancer);
+
 } else {
   store = createStore(Reducer, applyMiddleware(thunkMiddleware));
 }
